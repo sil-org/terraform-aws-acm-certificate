8d0d8b2fdc8e9ce2f5e8ed9e7d61a962006b81fe
briskt <3172830+briskt@users.noreply.github.com>
2025-12-10 20:54:58 +0800

    allow Cloudflare provider 4.x

--- versions.tf
+++ versions.tf
@@ -8,11 +8,8 @@ terraform {
       version = "~> 4.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
-
-      // 4.39.0 deprecated cloudflare_record.value
-      // While waiting for version 5 to mature, we'll constrain to earlier versions.
-      version = ">= 2.0.0, < 4.39.0"
+      source  = "cloudflare/cloudflare"
+      version = ">= 2.0.0, < 5.0.0"
     }
   }
 }
